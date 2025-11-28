93044ea6cfcf087ed5d1915ed97ea4fe9814415c
ArZarLordOfMango <arzarchannel@gmail.com>
2025-11-28 21:38:49 +0100

    comments

--- crates/wuestite_ecs/src/event.rs
+++ crates/wuestite_ecs/src/event.rs
@@ -1,5 +1,7 @@
 use std::any::Any;
 
+/// Events are used to transfer information between systems.
+/// [EventHandler] is used to store Events trait objects.
 pub trait Event: Any + Send + Sync {
     fn as_any(&self) -> &dyn Any;
 }
@@ -19,14 +21,17 @@ impl EventHandler {
         Self { events: Vec::new() }
     }
 
+    /// Adds an [Event] to the [EventHandler].
     pub fn push<E: Event + 'static>(&mut self, event: E) {
         self.events.push(Box::new(event));
     }
 
-    pub fn remove(&mut self, index: usize) {
+    /// Removes an [Event] by index
+    pub fn remove_event(&mut self, index: usize) {
         self.events.remove(index);
     }
 
+    /// Get all specific event of type.
     pub fn get_events<E: Event>(&self) -> Option<Vec<(usize, &E)>> {
         let events: Vec<(usize, &E)> = self
             .events
@@ -42,11 +47,8 @@ impl EventHandler {
         }
     }
 
+    /// Get all [Event]
     pub fn get_all(&self) -> Vec<&dyn Event> {
         self.events.iter().map(|e| e.as_ref()).collect()
-    }
-
-    pub fn remove_event(&mut self, index: usize) {
-        self.events.remove(index);
     }
 }
